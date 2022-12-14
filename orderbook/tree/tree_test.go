89001c8783b0a0a61439b3ddff23e153b4c50dcd
e.zhydzetski <e.zhydzetski@gmail.com>
2022-12-14 13:48:14 +0300

    test(orderbook): bench tree

--- orderbook/tree/tree_test.go
+++ orderbook/tree/tree_test.go
@@ -1,7 +1,9 @@
 package tree
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -52,4 +54,74 @@ func TestTree(t *testing.T) {
 		require.Fail(t, "tree should be empty")
 		return IAStop
 	})
+}
+
+func BenchmarkSet(b *testing.B) {
+	newTree := func() *Tree[int, int] {
+		return New[int, int](func(a int, b int) int {
+			if a < b {
+				return -1
+			}
+			if a > b {
+				return 1
+			}
+			return 0
+		})
+	}
+
+	tests := []struct {
+		name      string
+		generator func() func() int
+	}{
+		{
+			"inc",
+			func() func() int {
+				x := 0
+				return func() int {
+					x++
+					return x
+				}
+			},
+		},
+		{
+			"dec",
+			func() func() int {
+				x := 0
+				return func() int {
+					x--
+					return x
+				}
+			},
+		},
+		{
+			"random",
+			func() func() int {
+				rand.Seed(time.Now().UnixNano())
+				return func() int {
+					return rand.Int()
+				}
+			},
+		},
+		{
+			"const",
+			func() func() int {
+				return func() int {
+					return 777
+				}
+			},
+		},
+	}
+
+	for _, test := range tests {
+		b.Run(test.name, func(b *testing.B) {
+			tree := newTree()
+			next := test.generator()
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				x := next()
+				tree.Set(x, x)
+			}
+		})
+	}
 }
