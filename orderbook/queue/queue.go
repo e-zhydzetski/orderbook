2941ef9dd0d4056e55095fe67f4c866605b5a880
e.zhydzetski <e.zhydzetski@gmail.com>
2022-12-14 11:15:25 +0300

    feat(orderbook): add obj pool for queue

--- orderbook/queue/queue.go
+++ orderbook/queue/queue.go
@@ -1,30 +1,43 @@
 package queue
 
+import "sync"
+
 type Node[T any] struct {
 	Data T
 	Next *Node[T]
 }
 
 func New[T any]() *Queue[T] {
-	return &Queue[T]{}
+	return &Queue[T]{
+		nodePool: &sync.Pool{
+			New: func() any {
+				return new(Node[T])
+			},
+		},
+	}
 }
 
 type Queue[T any] struct {
-	Head *Node[T]
-	Tail *Node[T]
+	Head     *Node[T]
+	Tail     *Node[T]
+	nodePool *sync.Pool
 }
 
 func (q *Queue[T]) Add(val T) {
-	nn := &Node[T]{
-		Data: val,
+	newNode := func() *Node[T] {
+		nn := q.nodePool.Get().(*Node[T])
+		nn.Data = val
+		nn.Next = nil
+		return nn
 	}
+
 	if q.Head == nil {
-		q.Head = nn
+		q.Head = newNode()
 		q.Tail = q.Head
 		return
 	}
-	q.Head.Next = nn
-	q.Head = nn
+	q.Head.Next = newNode()
+	q.Head = q.Head.Next
 }
 
 type IteratorAction byte
@@ -38,7 +51,7 @@ func (q *Queue[T]) Iterate(f func(val *T) IteratorAction) {
 	cur := q.Tail
 	for cur != nil {
 		action := f(&cur.Data)
-		if action != IARemoveAndContinue {
+		if action == IAStop {
 			break
 		}
 		// remove from queue
@@ -46,6 +59,7 @@ func (q *Queue[T]) Iterate(f func(val *T) IteratorAction) {
 		if q.Tail == nil {
 			q.Head = nil
 		}
+		q.nodePool.Put(cur)
 		// and get next
 		cur = q.Tail
 	}
